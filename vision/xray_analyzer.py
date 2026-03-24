270896ba4ee23b75efe43bee2de84868b5638c7b
Chetty Rohan <chettyrohan180@gmail.com>
2026-03-24 11:33:09 +0530

    feat: Add computer vision module with OpenCV integration

---
+++ vision/xray_analyzer.py
@@ -0,0 +1,9 @@
+class XRayAnalyzer:
+    def __init__(self):
+        pass
+        
+    def analyze(self, image_data):
+        # Dummy implementation
+        if image_data is None:
+            return "No X-ray provided"
+        return "Normal"
